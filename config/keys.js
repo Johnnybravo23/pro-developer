aa8fae51b8c820bce8798691de738bd741744296
unknown <Johnnybravo23>
2019-08-02 03:41:58 -0700

    creating the token to the login route

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://johnnybravo:abc123@ds153947.mlab.com:53947/pro-developer'
+    mongoURI: 'mongodb://johnnybravo:abc123@ds153947.mlab.com:53947/pro-developer',
+    secretOrKey: 'secret'
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 // load user model
 const User = require('../../models/User');
@@ -66,8 +68,21 @@ router.post('/login', (req, res) => {
         // check the password
         bcrypt.compare(password, user.password).then(isMatch => {
             if (isMatch) {
-                res.json({
-                    msg: 'Success'
+                // user matched
+                const payload = {
+                    id: user.id,
+                    name: user.name,
+                    avatar: user.avatar
+                } // check jwt payload
+
+                // sign token
+                jwt.sign(payload, keys.secretOrKey, {
+                    expiresIn: 3600
+                }, (err, token) => {
+                    res.json({
+                        success: true,
+                        token: 'Bearer ' + token
+                    });
                 });
             } else {
                 return res.status(400).json({
